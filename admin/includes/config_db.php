02acdf09cf5bb76a18184d99079ea6fe8b30f29a
Ameron666 <aisakebi69@gmail.com>
2024-04-26 16:31:46 +0300

    change gallery; add functions for gallery;

--- admin/includes/config_db.php
+++ admin/includes/config_db.php
@@ -2,8 +2,42 @@
 function getSchema()
 {
     return [
-        
 
+        'gallery' => [
+            'menuName' => 'gallery',
+            'fields' => [
+                'title' => [
+                    'name' => 'Название',
+                    'element' => 'input',
+                    'type' => 'text',
+                    'requery' => false,
+                ],
+                'img' => [
+                    'name' => 'Картинки',
+                    'element' => 'input',
+                    'type' => 'file',
+                    'required' => true,
+                ],
+            ],
+        ],
+
+        'video' => [
+            'menuName' => 'Video',
+            'fields' => [
+                'title' => [
+                    'name' => 'Название',
+                    'element' => 'input',
+                    'type' => 'text',
+                    'requery' => false,
+                ],
+                'img' => [
+                    'name' => 'Видео',
+                    'element' => 'input',
+                    'type' => 'file',
+                    'required' => true,
+                ],
+            ],
+        ],
 
         'comment' => [
             'menuName' => 'Отзывы',
@@ -23,6 +57,6 @@ function getSchema()
                 ],
             ],
         ],
-    
+
     ];
 }
--- edit.js
+++ edit.js
@@ -32,20 +32,97 @@ function getFileExtension(filename) {
 
 // Функция для отображения всплывающего окна с сообщением
 function showPopup(message, success) {
-  var popup = document.getElementById('popup');
-  var popupMessage = document.getElementById('popup-message');
-  success ? $(".popup-content").css('background', 'green') : $(".popup-content").css('background', 'red');
+  var popup = document.getElementById("popup");
+  var popupMessage = document.getElementById("popup-message");
+  success
+    ? $(".popup-content").css("background", "green")
+    : $(".popup-content").css("background", "red");
 
   popupMessage.innerHTML = message; // Устанавливаем сообщение во всплывающее окно
-  popup.style.display = 'flex'; // Отображаем всплывающее окно
+  popup.style.display = "flex"; // Отображаем всплывающее окно
 }
 
 // Функция для закрытия всплывающего окна
 function closePopup() {
-  var popup = document.getElementById('popup');
-  popup.style.display = 'none'; // Скрываем всплывающее окно
+  var popup = document.getElementById("popup");
+  popup.style.display = "none"; // Скрываем всплывающее окно
 }
 
 // ----------------------------------------------------------------
+let modal = $(".modal-content").empty();
 
+getData("gallery").then((response) => {
 
+  let mass = [];
+
+  response.forEach((element) => {
+    mass.push(stringToImageArray(element.img));
+  });
+
+  let block = $(".gallery_full").empty();
+  let fullWidthCount = 0;
+
+  mass.forEach((element) => {
+    if (fullWidthCount == 0) {
+      block.append(`
+            <img src="admin/img/${element}" alt="" class="fullWidth">
+        `);
+    } else if (fullWidthCount != 5 && fullWidthCount != 6) {
+      block.append(`
+            <img src="admin/img/${element}" alt="" class="halfWidth">
+        `);
+    } else if (fullWidthCount == 5) {
+      block.append(`
+            <img src="admin/img/${element}" alt="" class="fullWidth">
+        `);
+    } else if (fullWidthCount == 6) {
+      block.append(`
+            <img src="admin/img/${element}" alt="" class="fullWidth">
+        `);
+      fullWidthCount = 0;
+    }
+
+    fullWidthCount++;
+  });
+});
+
+// ------------------------------------------------------------------------------------------------
+
+getData("gallery").then((response) => {
+  let mass = [];
+  let imgsMax = 9;
+  let imgsCount = 0;
+
+  response.forEach((element) => {
+    if (imgsCount < imgsMax) {
+      mass.push(stringToImageArray(element.img));
+      imgsCount++;
+    }
+  });
+
+  let block = $(".gallery_main").empty();
+  let fullWidthCount = 0;
+
+  mass.forEach((element) => {
+    if (fullWidthCount == 0) {
+      block.append(`
+            <img src="admin/img/${element}" alt="" class="fullWidth">
+        `);
+    } else if (fullWidthCount != 5 && fullWidthCount != 6) {
+      block.append(`
+            <img src="admin/img/${element}" alt="" class="halfWidth">
+        `);
+    } else if (fullWidthCount == 5) {
+      block.append(`
+            <img src="admin/img/${element}" alt="" class="fullWidth">
+        `);
+    } else if (fullWidthCount == 6) {
+      block.append(`
+            <img src="admin/img/${element}" alt="" class="fullWidth">
+        `);
+      fullWidthCount = 0;
+    }
+
+    fullWidthCount++;
+  });
+});
